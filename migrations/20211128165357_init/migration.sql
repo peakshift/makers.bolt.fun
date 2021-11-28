0d0e57baaff4780901042dc9583dd9c049f2c4cf
Michael Bumann <hello@michaelbumann.com>
2021-11-28 10:55:04 -0600

    update database for votes

--- migrations/20211128165357_init/migration.sql
+++ migrations/20211128165357_init/migration.sql
@@ -12,13 +12,29 @@ CREATE TABLE "Project" (
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "website" TEXT NOT NULL,
-    "thumbnail_image" TEXT NOT NULL,
-    "cover_image" TEXT NOT NULL,
+    "thumbnail_image" TEXT,
+    "cover_image" TEXT,
     "category_id" INTEGER NOT NULL,
     "votes_count" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" SERIAL NOT NULL,
+    "project_id" INTEGER NOT NULL,
+    "amount_in_sat" INTEGER NOT NULL,
+    "payment_request" TEXT,
+    "payment_hash" TEXT,
+    "preimage" TEXT,
+    "paid" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
 -- AddForeignKey
 ALTER TABLE "Project" ADD CONSTRAINT "Project_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
