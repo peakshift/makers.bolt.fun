44d1285681c9e09fadd799a7b546be532257cd37
MTG2000 <mtg0987654321@gmail.com>
2022-01-15 11:04:18 +0200

    fix: remove unimplemeted links from mobile navbar

--- src/Components/Navbar/NavMobile.tsx
+++ src/Components/Navbar/NavMobile.tsx
@@ -47,15 +47,17 @@ export default function NavMobile({ onSearch }: Props) {
     return (
         <nav className='block bg-white fixed top-0 left-0 w-full lg:hidden overflow-hidden z-[2010]'>
             <div className="p-16 px-32 w-screen flex justify-center items-center">
-                <div className="w-40 h-40 bg-gray-100 rounded-8 mr-auto overflow-hidden">
+                {/* <div className="w-40 h-40 bg-gray-100 rounded-8 mr-auto overflow-hidden">
                     <img className="w-full h-full object-cover" src="https://www.figma.com/file/OFowr5RJk9YZCW35KT7D5K/image/07b85d84145942255afd215b3da26dbbf1dd03bd?fuid=772401335362859303" alt="" />
-                </div>
+                </div> */}
                 <Link to='/'><h2 className="text-h5 font-bold mx-auto">makers.bolt.fun</h2></Link>
-                <button className='rounded-full ml-auto text-2xl w-[50px] h-[50px] hover:bg-gray-200' onClick={handleClick}>
+
+                <Button size='sm' color='primary' className=" rounded-24 ml-auto">Submit App️</Button>
+                {/* <button className='rounded-full ml-auto text-2xl w-[50px] h-[50px] hover:bg-gray-200' onClick={handleClick}>
 
                     {!open ? (<motion.div key={open ? 1 : 0} variants={navBtnVariant} initial='menuHide' animate='menuShow'><FiMenu /></motion.div>)
                         : (<motion.div key={open ? 1 : 0} variants={navBtnVariant} initial='closeHide' animate='closeShow'><GrClose /></motion.div>)}
-                </button>
+                </button> */}
             </div>
 
             <div className="fixed overflow-hidden left-0 pointer-events-none z-[2010] w-full min-h-[calc(100vh-76px)]">
