42da5c257ebf67c7ce2578e1586f11b8c9c55cbb
MTG2000 <mtg0987654321@gmail.com>
2022-02-03 19:01:41 +0200

    remove console.log

--- src/pages/ExplorePage/ProjectsRow/ProjectsRow.tsx
+++ src/pages/ExplorePage/ProjectsRow/ProjectsRow.tsx
@@ -48,8 +48,6 @@ export default function ProjectsRow({ title, categoryId, projects }: Props) {
     }
 
     useResizeListener(() => {
-        console.log(calcNumItems());
-
         setCarouselItmsCnt(calcNumItems());
     }, [setCarouselItmsCnt])
 
