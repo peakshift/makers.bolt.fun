28410541ed0a7224c292085c29619fa738ad73da
MTG2000 <mtg0987654321@gmail.com>
2022-08-07 16:19:26 +0300

    refactor: extract common carousel logic to custom useCarousel hook, hide carousel arrows if can't scroll more

--- src/features/Projects/pages/ExplorePage/Categories/Categories.tsx
+++ src/features/Projects/pages/ExplorePage/Categories/Categories.tsx
@@ -1,9 +1,8 @@
 
-import useEmblaCarousel from 'embla-carousel-react'
 import { useNavigate } from 'react-router-dom';
 import { useAllCategoriesQuery } from 'src/graphql';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import { useCallback } from 'react';
+import { useCarousel } from 'src/utils/hooks';
 
 const colors = [
     '#FDF2F8',
@@ -20,17 +19,10 @@ const colors = [
 
 export default function Categories() {
 
-    const [emblaRef, emblaApi] = useEmblaCarousel({ align: 'start', slidesToScroll: 2 })
+    const { viewportRef, scrollSlides, canScrollNext, canScrollPrev, isClickAllowed } = useCarousel({ align: 'start', slidesToScroll: 2 })
     const { data, loading } = useAllCategoriesQuery();
     const navigate = useNavigate();
 
-    const scrollPrev = useCallback(() => {
-        if (emblaApi) emblaApi.scrollPrev()
-    }, [emblaApi])
-
-    const scrollNext = useCallback(() => {
-        if (emblaApi) emblaApi.scrollNext()
-    }, [emblaApi])
 
     if (loading || !data)
         return <div className="flex gap-12">
@@ -48,22 +40,22 @@ export default function Categories() {
     return (
 
         <div className="relative group">
-            <div className="overflow-hidden" ref={emblaRef}>
+            <div className="overflow-hidden" ref={viewportRef}>
                 <div className="select-none w-full flex gap-16">
                     {data?.allCategories.map((category, idx) =>
                         <button
                             key={category.id}
-                            onClick={() => emblaApi?.clickAllowed() && navigate(`/apps/category/${category.id}`)}
+                            onClick={() => isClickAllowed() && navigate(`/apps/category/${category.id}`)}
                             className='min-w-max block p-16 rounded-16 hover:bg-gray-100 active:bg-gray-200 active:scale-90 transition-transform'
                             style={{ backgroundColor: colors[idx % colors.length] }}
                         >{category.icon} {category.title}</button>
                     )}
                 </div>
             </div>
-            <button className="absolute inset-y-0 w-42 left-0 opacity-0 group-hover:opacity-100 transition-opacity rounded-l-12 bg-gradient-to-r from-gray-700 text-white" onClick={scrollPrev}>
+            <button className={`absolute inset-y-0 w-42 left-0 opacity-0 ${canScrollPrev && 'group-hover:opacity-100'} transition-opacity rounded-l-12 bg-gradient-to-r from-gray-700 text-white`} onClick={() => scrollSlides(-1)}>
                 <FaChevronLeft />
             </button>
-            <button className="absolute inset-y-0 w-42 right-0 opacity-0 group-hover:opacity-100 transition-opacity rounded-r-12 bg-gradient-to-l from-gray-700 text-white" onClick={scrollNext}>
+            <button className={`absolute inset-y-0 w-42 right-0 opacity-0 ${canScrollNext && 'group-hover:opacity-100'} transition-opacity rounded-r-12 bg-gradient-to-l from-gray-700 text-white`} onClick={() => scrollSlides(1)}>
                 <FaChevronRight />
             </button>
         </div>
--- src/features/Projects/pages/ExplorePage/ProjectsRow/ProjectsRow.tsx
+++ src/features/Projects/pages/ExplorePage/ProjectsRow/ProjectsRow.tsx
@@ -1,11 +1,10 @@
-import { ReactNode, useCallback, } from "react";
+import { ReactNode, useCallback, useEffect, useRef, useState, } from "react";
 import { ProjectCard } from "src/utils/interfaces";
 import { MdDoubleArrow, } from 'react-icons/md';
-import { useAppDispatch } from "src/utils/hooks";
+import { useAppDispatch, useCarousel, useResizeListener } from "src/utils/hooks";
 import { openModal } from "src/redux/features/modals.slice";
 import { Link } from "react-router-dom";
 import ProjectCardMini from "src/features/Projects/Components/ProjectCardMini/ProjectCardMini";
-import useEmblaCarousel from 'embla-carousel-react'
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 interface Props {
@@ -21,25 +20,33 @@ function calcNumItems(width = Math.min(window.innerWidth - 32, 1440)) {
 }
 
 
+
+
 export default function ProjectsRow({ title, link, projects }: Props) {
 
-    const [emblaRef, emblaApi] = useEmblaCarousel({ align: 'start' })
+    const [slidesToScroll, setSlidesToScroll] = useState(1)
+    const rowRef = useRef<HTMLDivElement>(null)
+    const { viewportRef, scrollSlides, canScrollNext, canScrollPrev, isClickAllowed } = useCarousel({ align: 'start', slidesToScroll })
     const dispatch = useAppDispatch()
 
 
+    const recalcSlidesToScroll = useCallback(() => {
+        if (rowRef.current)
+            setSlidesToScroll(Math.floor(calcNumItems(rowRef.current.clientWidth)))
+    }, [])
 
-
-    const scrollSlides = useCallback((direction = 1) => {
-        if (emblaApi) emblaApi.scrollTo(emblaApi.selectedScrollSnap() + direction * Math.floor(calcNumItems()))
-    }, [emblaApi])
+    useEffect(recalcSlidesToScroll, [recalcSlidesToScroll])
+    useResizeListener(recalcSlidesToScroll);
 
 
     if (projects.length === 0)
         return <></>
 
 
+
+
     const handleClick = (projectId: number) => {
-        if (emblaApi?.clickAllowed()) {
+        if (isClickAllowed()) {
             dispatch(openModal({ Modal: "ProjectDetailsCard", props: { projectId } }))
         }
     }
@@ -54,8 +61,8 @@ export default function ProjectsRow({ title, link, projects }: Props) {
                     <MdDoubleArrow className='text-gray-200 ml-8 hover:cursor-pointer transform scale-y-110 scale-x-125 origin-left' />
                 </Link>}
             </h3>
-            <div className="relative group">
-                <div className="overflow-hidden" ref={emblaRef}>
+            <div className="relative group" ref={rowRef}>
+                <div className="overflow-hidden" ref={viewportRef}>
                     <div className="w-full flex gap-16">
                         {projects.map((project, idx) =>
                             <div key={project.id} className='flex-[0_0_100%] max-w-[296px]' >
@@ -64,13 +71,25 @@ export default function ProjectsRow({ title, link, projects }: Props) {
                         )}
                     </div>
                 </div>
-                <button className="absolute inset-y-0 w-64 left-0 opacity-0 group-hover:opacity-100 transition-opacity rounded-l-12 bg-gradient-to-r from-gray-700 text-white" onClick={() => scrollSlides(-1)}>
+                <button
+                    className={`absolute inset-y-0 w-64 left-0 opacity-0  transition-opacity 
+                                rounded-l-12 bg-gradient-to-r from-gray-700 text-white
+                                ${canScrollPrev ? "group-hover:opacity-100" : ""}
+                                `}
+                    onClick={() => scrollSlides(-1)}
+                >
                     <FaChevronLeft />
                 </button>
-                <button className="absolute inset-y-0 w-64 right-0 opacity-0 group-hover:opacity-100 transition-opacity rounded-r-12 bg-gradient-to-l from-gray-700 text-white" onClick={() => scrollSlides(1)}>
+                <button
+                    className={`absolute inset-y-0 w-64 right-0 opacity-0  transition-opacity 
+                                rounded-r-12 bg-gradient-to-l from-gray-700 text-white
+                                ${canScrollNext ? "group-hover:opacity-100" : ""}
+                                `}
+                    onClick={() => scrollSlides(1)}
+                >
                     <FaChevronRight />
                 </button>
             </div>
-        </div>
+        </div >
     )
 }
