709392996c569c90721839937b2ee44c4bfb6ac1
MTG2000 <mtg0987654321@gmail.com>
2022-08-17 09:51:20 +0300

    update: db schema

--- api/functions/login/login.js
+++ api/functions/login/login.js
@@ -84,6 +84,7 @@ const loginHandler = async (req, res) => {
             await prisma.userKey.create({
                 data: {
                     key,
+                    name: "My original wallet key",
                     user_id: createdUser.id,
                 }
             });
