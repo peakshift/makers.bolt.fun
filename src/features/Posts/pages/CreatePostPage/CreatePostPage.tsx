db295f0cd630e5c0cc38275eaefd08e514c73af4
MTG2000 <mtg0987654321@gmail.com>
2022-09-14 14:21:56 +0300

    fix: handle wrong image format in create-story cache

--- src/features/Posts/pages/CreatePostPage/CreatePostPage.tsx
+++ src/features/Posts/pages/CreatePostPage/CreatePostPage.tsx
@@ -22,39 +22,41 @@ export default function CreatePostPage() {
             {postType === 'bounty' && <title>Create Bounty</title>}
             {postType === 'question' && <title>Create Question</title>}
         </Helmet>
-        <div
-            className="page-container grid gap-24 grid-cols-1 lg:grid-cols-[1fr_4fr]"
-        // style={{ gridTemplateColumns: "326px 1fr" }}
-        >
-            <div className="">
-                {/* <PostTypeList selectionChanged={setPostType} /> */}
-                <button className={`
+        <div className="page-container">
+            <div
+                className="grid gap-24 grid-cols-1 lg:grid-cols-[1fr_4fr]"
+            // style={{ gridTemplateColumns: "326px 1fr" }}
+            >
+                <div className="">
+                    {/* <PostTypeList selectionChanged={setPostType} /> */}
+                    <button className={`
                 w-48 aspect-square bg-white rounded-12 border-2 border-gray-200 justify-around items-center text-gray-900 hover:bg-gray-50 active:bg-gray-100
                 `}
-                    onClick={() => navigate(-1)}
-                >
-                    <FiArrowLeft className={"text-body3"} />
-                </button>
-            </div>
-            <div  >
-                {postType === 'story' && <>
-                    {/* <h2 className="text-h2 font-bolder text-gray-800 mb-32">
+                        onClick={() => navigate(-1)}
+                    >
+                        <FiArrowLeft className={"text-body3"} />
+                    </button>
+                </div>
+                <div  >
+                    {postType === 'story' && <>
+                        {/* <h2 className="text-h2 font-bolder text-gray-800 mb-32">
                         Write a Story
                     </h2> */}
-                    <CreateStoryPage />
-                </>}
-                {postType === 'bounty' && <>
-                    <h2 className="text-h2 font-bolder text-gray-800 mb-32">
-                        Write a Bounty
-                    </h2>
-                    <BountyForm />
-                </>}
-                {postType === 'question' && <>
-                    <h2 className="text-h2 font-bolder text-gray-800 mb-32">
-                        Write a Question
-                    </h2>
-                    <QuestionForm />
-                </>}
+                        <CreateStoryPage />
+                    </>}
+                    {postType === 'bounty' && <>
+                        <h2 className="text-h2 font-bolder text-gray-800 mb-32">
+                            Write a Bounty
+                        </h2>
+                        <BountyForm />
+                    </>}
+                    {postType === 'question' && <>
+                        <h2 className="text-h2 font-bolder text-gray-800 mb-32">
+                            Write a Question
+                        </h2>
+                        <QuestionForm />
+                    </>}
+                </div>
             </div>
         </div>
     </>
