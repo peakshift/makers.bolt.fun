11f81dfd1f9ff315487cbae5f37bd1e8f91c5652
MTG2000 <mtg0987654321@gmail.com>
2022-10-06 15:46:41 +0300

    update: fix errors

--- src/features/Projects/pages/ProjectPage/ProjectDetailsCard/ProjectDetailsCard.tsx
+++ src/features/Projects/pages/ProjectPage/ProjectDetailsCard/ProjectDetailsCard.tsx
@@ -7,7 +7,7 @@ import { setProject } from 'src/redux/features/project.slice';
 import Button from 'src/Components/Button/Button';
 import ProjectCardSkeleton from './ProjectDetailsCard.Skeleton'
 import { NotificationsService, Wallet_Service } from 'src/services'
-import { ProjectLaunchStatusEnum, ProjectPermissionEnum, useProjectDetailsQuery } from 'src/graphql';
+import { useProjectDetailsQuery } from 'src/graphql';
 import Lightbox from 'src/Components/Lightbox/Lightbox'
 import linkifyHtml from 'linkify-html';
 import ErrorMessage from 'src/Components/Errors/ErrorMessage/ErrorMessage';
@@ -78,44 +78,33 @@ export default function ProjectDetailsCard({ direction, projectId, ...props }: P
 
     const links = [
         {
-            value: project.discord,
-            text: project.discord,
+            value: project?.discord,
+            text: project?.discord,
             icon: FaDiscord,
             colors: "bg-violet-100 text-violet-900",
         },
         {
-            value: project.website,
-            text: project.website?.replace(/(^\w+:|^)\/\//, '').replace(/\/$/, ""),
+            value: project?.website,
+            text: project?.website?.replace(/(^\w+:|^)\/\//, '').replace(/\/$/, ""),
             icon: FiGlobe,
             colors: "bg-gray-100 text-gray-900",
-            url: project.website
+            url: project?.website
         },
         {
-            value: project.twitter,
-            text: project.twitter,
+            value: project?.twitter,
+            text: project?.twitter,
             icon: FiTwitter,
             colors: "bg-blue-100 text-blue-500",
-            url: project.twitter
+            url: project?.twitter
         },
         {
-            value: project.repository,
-            text: project.repository,
+            value: project?.repository,
+            text: project?.repository,
             icon: FiGithub,
             colors: "bg-pink-100 text-pink-600",
-            url: project.repository
+            url: project?.repository
         },
     ];
-
-    const onVote = (votes?: number) => {
-        dispatch(setVoteAmount(votes ?? 10));
-        // dispatch(openModal({
-        //     Modal: 'VoteCard', props: {
-        //         projectId: project.id,
-        //         title: project.title,
-        //         initVotes: votes
-        //     }
-        // }))
-    }
 
     return (
         <div
@@ -123,10 +112,10 @@ export default function ProjectDetailsCard({ direction, projectId, ...props }: P
         >
             {/* Cover Image */}
             <div className="relative h-[120px] lg:h-[80px]">
-                <img className="w-full h-full object-cover" src={project.cover_image} alt="" />
+                {/* <img className="w-full h-full object-cover" src={project?.cover_image} alt="" /> */}
                 <div className="absolute w-full px-16 md:px-24 top-16 md:top-24 flex justify-between items-center">
                     <div className="flex gap-8 bg-gray-800 bg-opacity-60 text-white rounded-48 py-4 px-12 text-body6 font-medium">
-                        {project.yearFounded != "" && `YEAR_FOUNDED_PLACEHOLDER`}
+                        {project?.yearFounded && `Year founded ${project.yearFounded}`}
                     </div>
                     <div className="flex gap-8">
                         <button className="w-32 h-32  bg-gray-800 bg-opacity-60 text-white rounded-full hover:bg-opacity-40 text-center flex flex-col justify-center items-center" onClick={closeModal}><IoMdClose className=' inline-block' /></button>
@@ -141,9 +130,9 @@ export default function ProjectDetailsCard({ direction, projectId, ...props }: P
                         <img className="w-full h-full border-2 border-white rounded-24" src={project?.logo?.[0]['thumbnails']['large'].url} alt="" />
                     </div>
                     <div className='flex flex-col gap-8 items-start justify-between'>
-                        <a href={project.website} target='_blank' rel="noreferrer"><h3 className="text-body1 font-bold">{project.title}</h3></a>
+                        <a href={project?.website!} target='_blank' rel="noreferrer"><h3 className="text-body1 font-bold">{project?.title}</h3></a>
                         <div>
-                            <span className="font-medium text-body4 text-gray-600">{project.category}</span>
+                            <span className="font-medium text-body4 text-gray-600">{project?.category}</span>
                         </div>
                     </div>
                 </div>
@@ -152,14 +141,9 @@ export default function ProjectDetailsCard({ direction, projectId, ...props }: P
                 {/* About */}
                 <div>
                     <p className="text-body6 uppercase font-medium text-gray-400 mb-8">About</p>
-                    <div className=" text-body4 text-gray-600 leading-normal whitespace-pre-line" dangerouslySetInnerHTML={{
-                        __html: linkifyHtml(project.description, {
-                            className: ' text-blue-500 underline',
-                            defaultProtocol: 'https',
-                            target: "_blank",
-                            rel: 'noreferrer'
-                        })
-                    }}></div>
+                    <div className=" text-body4 text-gray-600 leading-normal whitespace-pre-line">
+                        {project?.description}
+                    </div>
 
                     {/* Links */}
                     <div className="mt-16 flex flex-wrap gap-16">
@@ -189,11 +173,11 @@ export default function ProjectDetailsCard({ direction, projectId, ...props }: P
                     </div>
                 </div>
 
-                {/*project.capabilities.length > 0 &&
+                {/*project?.capabilities.length > 0 &&
                     <div>
                         <p className="text-body6 uppercase font-medium text-gray-400 mb-8">CAPABILITIES</p>
                         <div className="flex flex-wrap gap-8">
-                            {project.capabilities.map(cap => <Badge key={cap.id} size='sm'>{cap.icon} {cap.title}</Badge>)}
+                            {project?.capabilities.map(cap => <Badge key={cap.id} size='sm'>{cap.icon} {cap.title}</Badge>)}
                         </div>
                     </div>*/}
 
@@ -203,7 +187,7 @@ export default function ProjectDetailsCard({ direction, projectId, ...props }: P
                         color='gray'
                         size='md'
                         className="my-16"
-                        href={`https://airtable.com/shr67F20KG9Gdok6d?prefill_app_name=${project.title}&prefill_app_link=${project.website}`}
+                        href={`https://airtable.com/shr67F20KG9Gdok6d?prefill_app_name=${project?.title}&prefill_app_link=${project?.website}`}
                         newTab
                     // onClick={onClaim}
                     >Request Edit 📝</Button>
