9e37d1374bb3924d2625b9596cd9671ea0f1bd37
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-01-05 11:37:40 +0400

    update (explore header): change tournament btn text

--- src/features/Projects/pages/ExplorePage/Header/Header.tsx
+++ src/features/Projects/pages/ExplorePage/Header/Header.tsx
@@ -1,32 +1,42 @@
-
 import Assets from "src/assets";
 import Button from "src/Components/Button/Button";
 import { MEDIA_QUERIES } from "src/utils/theme/media_queries";
 import CustomDot from "./CustomDot/CustomDot";
-import useEmblaCarousel from 'embla-carousel-react'
+import useEmblaCarousel from "embla-carousel-react";
 import { useCallback, useEffect, useState } from "react";
 import { createRoute } from "src/utils/routing";
 import { IoLocationOutline } from "react-icons/io5";
 
 export const bannerData = {
-  title:
+  title: (
     <>
-      <p className="text-body1 font-bolder text-white">Legends of Lightning ⚡️ Tournament</p>
-      <p className="text-body3 font-medium text-white mt-8">12th Oct - 7th Dec, 2022</p>
-      <p className="text-body4 text-white mt-8"><IoLocationOutline className="mr-8" /> Online</p>
-    </>,
+      <p className="text-body1 font-bolder text-white">
+        Legends of Lightning ⚡️ Tournament
+      </p>
+      <p className="text-body3 font-medium text-white mt-8">
+        12th Oct - 7th Dec, 2022
+      </p>
+      <p className="text-body4 text-white mt-8">
+        <IoLocationOutline className="mr-8" /> Online
+      </p>
+    </>
+  ),
   img: "https://i.ibb.co/3S35g6T/wide.jpg",
   link: {
-    content: "Register Now",
+    content: "View Winners",
     url: createRoute({ type: "tournament", id: 1 }),
-    newTab: false
+    newTab: false,
   },
-}
+};
 
 const headerLinks = [
   bannerData,
   {
-    title: <p className="text-body1 font-bolder text-white">Explore a fun directory of lightning web apps</p>,
+    title: (
+      <p className="text-body1 font-bolder text-white">
+        Explore a fun directory of lightning web apps
+      </p>
+    ),
     img: Assets.Images_ExploreHeader1,
     link: {
       content: "Submit project",
@@ -36,18 +46,15 @@ const headerLinks = [
   },
 ];
 
-
-
 export default function Header() {
-
   const [emblaRef, emblaApi] = useEmblaCarousel({
-    align: 'start',
+    align: "start",
     breakpoints: {
       [MEDIA_QUERIES.isMedium]: {
-        draggable: false
-      }
-    }
-  })
+        draggable: false,
+      },
+    },
+  });
 
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
@@ -75,10 +82,12 @@ export default function Header() {
               alt=""
             />
             <div className="w-full h-full object-cover bg-gradient-to-t from-gray-900 absolute top-0 left-0 z-[-1]"></div>
-            <div className="max-w-[90%]">
-              {headerLinks[0].title}
-            </div>
-            <Button color="white" href={headerLinks[0].link.url} className="mt-24">
+            <div className="max-w-[90%]">{headerLinks[0].title}</div>
+            <Button
+              color="white"
+              href={headerLinks[0].link.url}
+              className="mt-24"
+            >
               {headerLinks[0].link.content}
             </Button>
           </div>
@@ -89,10 +98,13 @@ export default function Header() {
               alt=""
             />
             <div className="w-full h-full object-cover bg-gradient-to-t from-gray-900 absolute top-0 left-0 z-[-1]"></div>
-            <div className="max-w-[90%]">
-              {headerLinks[1].title}
-            </div>
-            <Button color="white" href={headerLinks[1].link.url} newTab={headerLinks[1].link.newTab ?? false} className="mt-24">
+            <div className="max-w-[90%]">{headerLinks[1].title}</div>
+            <Button
+              color="white"
+              href={headerLinks[1].link.url}
+              newTab={headerLinks[1].link.newTab ?? false}
+              className="mt-24"
+            >
               {headerLinks[1].link.content}
             </Button>
           </div>
@@ -100,13 +112,9 @@ export default function Header() {
       </div>
       <div className="absolute inset-x-0 bottom-8 flex justify-center gap-4 md:hidden">
         {scrollSnaps.map((_, index) => (
-          <CustomDot
-            key={index}
-            active={index === selectedIndex}
-          />
+          <CustomDot key={index} active={index === selectedIndex} />
         ))}
       </div>
     </div>
-
   );
 }
