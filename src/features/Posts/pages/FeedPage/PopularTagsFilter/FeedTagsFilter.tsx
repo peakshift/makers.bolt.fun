c2d2b8255ae1a46c15ba29de20475bd2c839b24d
Johns Beharry <johns@peakshift.com>
2023-01-09 18:31:43 +0100

    styles(feed): remove card styles from trending and tags section

--- src/features/Posts/pages/FeedPage/PopularTagsFilter/FeedTagsFilter.tsx
+++ src/features/Posts/pages/FeedPage/PopularTagsFilter/FeedTagsFilter.tsx
@@ -36,99 +36,97 @@ export default function FeedTagsFilter({ value, onChange }: Props) {
   return (
     <div className="overflow-hidden">
       {isMdScreen ? (
-        <Card>
-          <p className="text-body2 font-bolder text-black mb-16">🏷️ Tags</p>
-          <ul className=" flex flex-col gap-16">
-            {tagsQuery.loading
-              ? Array(10)
-                  .fill(0)
-                  .map((_, idx) => (
-                    <li
-                      key={idx}
-                      className={`flex items-start rounded-8 font-bold p-4`}
-                    >
-                      <span className="bg-gray-50 rounded-8 w-40 h-40 text-center py-8">
-                        {" "}
-                      </span>
-                      <span className="self-center px-16">
-                        <Skeleton width={"7ch"} />
-                      </span>
-                    </li>
-                  ))
-              : tagsQuery.data?.officialTags
-                  .slice(0, showingAll ? -1 : MAX_SHOWED_TAGS)
-                  .map((tag) => (
-                    <li key={tag.id}>
-                      <Link
-                        to={createRoute({ type: "tag-page", tag: tag.title })}
-                        className={`flex items-start rounded-8 cursor-pointer font-bold p-4
-                                 active:scale-95 transition-transform
-                                ${
-                                  tag.id === selectedId
-                                    ? "bg-gray-200"
-                                    : "hover:bg-gray-100"
-                                }
-                                `}
-                      >
-                        <span
-                          className={`${
-                            tag.id !== selectedId && "bg-gray-50"
-                          } rounded-8 w-40 h-40 text-center py-8`}
-                        >
-                          {tag.icon}
-                        </span>
-                        <span className="self-center px-16">
-                          {formatHashtag(tag.title)}
-                        </span>
-                      </Link>
-                    </li>
-                  ))}
-          </ul>
-          {tagsQuery.data &&
-            tagsQuery.data?.officialTags.length > MAX_SHOWED_TAGS && (
-              <Button
-                fullWidth
-                size="sm"
-                variant="text"
-                className="text-blue-400 mt-16"
-                onClick={() => setShowingAll((v) => !v)}
-              >
-                {showingAll ? "Show less" : "Show more"}
-              </Button>
-            )}
-        </Card>
-      ) : (
-        <>
-          {tagsQuery.loading ? (
-            <ul className="flex gap-8 ">
-              {Array(4)
+        <p className="text-body2 font-bolder text-black mb-16">🏷️ Tags</p>
+        <ul className=" flex flex-col gap-16">
+          {tagsQuery.loading
+            ? Array(10)
                 .fill(0)
                 .map((_, idx) => (
-                  <div
+                  <li
                     key={idx}
-                    className="py-12 px-16 bg-gray-100 rounded-8 text-body5"
+                    className={`flex items-start rounded-8 font-bold p-4`}
                   >
-                    <span className="opacity-0">Category</span>
-                  </div>
+                    <span className="bg-gray-50 rounded-8 w-40 h-40 text-center py-8">
+                      {" "}
+                    </span>
+                    <span className="self-center px-16">
+                      <Skeleton width={"7ch"} />
+                    </span>
+                  </li>
+                ))
+            : tagsQuery.data?.officialTags
+                .slice(0, showingAll ? -1 : MAX_SHOWED_TAGS)
+                .map((tag) => (
+                  <li key={tag.id}>
+                    <Link
+                      to={createRoute({ type: "tag-page", tag: tag.title })}
+                      className={`flex items-start rounded-8 cursor-pointer font-bold p-4
+                               active:scale-95 transition-transform
+                              ${
+                                tag.id === selectedId
+                                  ? "bg-gray-200"
+                                  : "hover:bg-gray-100"
+                              }
+                              `}
+                    >
+                      <span
+                        className={`${
+                          tag.id !== selectedId && "bg-gray-50"
+                        } rounded-8 w-40 h-40 text-center py-8`}
+                      >
+                        {tag.icon}
+                      </span>
+                      <span className="self-center px-16">
+                        {formatHashtag(tag.title)}
+                      </span>
+                    </Link>
+                  </li>
                 ))}
-            </ul>
-          ) : (
-            <Slider>
-              {tagsQuery.data?.officialTags.map((tag) => (
-                <Link
-                  key={tag.id}
-                  to={createRoute({ type: "tag-page", tag: tag.title })}
-                  className={`${
-                    tag.id === selectedId ? "bg-gray-200" : "bg-gray-100"
-                  } py-12 px-16 rounded-8 text-body5`}
-                >
-                  {tag.icon} {formatHashtag(tag.title)}
-                </Link>
-              ))}
-            </Slider>
+        </ul>
+        {tagsQuery.data &&
+          tagsQuery.data?.officialTags.length > MAX_SHOWED_TAGS && (
+            <Button
+              fullWidth
+              size="sm"
+              variant="text"
+              className="text-blue-400 mt-16"
+              onClick={() => setShowingAll((v) => !v)}
+            >
+              {showingAll ? "Show less" : "Show more"}
+            </Button>
           )}
-        </>
-      )}
-    </div>
+      </Card>
+    ) : (
+      <>
+        {tagsQuery.loading ? (
+          <ul className="flex gap-8 ">
+            {Array(4)
+              .fill(0)
+              .map((_, idx) => (
+                <div
+                  key={idx}
+                  className="py-12 px-16 bg-gray-100 rounded-8 text-body5"
+                >
+                  <span className="opacity-0">Category</span>
+                </div>
+              ))}
+          </ul>
+        ) : (
+          <Slider>
+            {tagsQuery.data?.officialTags.map((tag) => (
+              <Link
+                key={tag.id}
+                to={createRoute({ type: "tag-page", tag: tag.title })}
+                className={`${
+                  tag.id === selectedId ? "bg-gray-200" : "bg-gray-100"
+                } py-12 px-16 rounded-8 text-body5`}
+              >
+                {tag.icon} {formatHashtag(tag.title)}
+              </Link>
+            ))}
+          </Slider>
+        )}
+      </>
+    )}
   );
 }
