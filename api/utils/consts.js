488f75e3803625e7c3da2f542de20aaa2ba4c6e6
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-01-26 18:12:13 +0400

    debugging

--- api/utils/consts.js
+++ api/utils/consts.js
@@ -15,7 +15,7 @@ const env = envsafe(
       devDefault: process.env.URL ?? "http://localhost:8888",
     }),
     NETLIFY: bool({
-      default: false,
+      default: !!process.env.NETLIFY,
       devDefault: process.env.NETLIFY_DEV,
     }),
     JWT_SECRET: str(),
