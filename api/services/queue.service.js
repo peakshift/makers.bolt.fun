db66a3d5bc85e6bf1b4ad178121be9627abd5d71
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-02-14 17:59:04 +0400

    update (queue): send tags to queue_service

--- api/services/queue.service.js
+++ api/services/queue.service.js
@@ -1,11 +1,18 @@
 const { default: axios } = require("axios");
 const env = require("../utils/consts");
 
-function createStoryRootEvent({ id, title, url, canonical_url, author_name }) {
+function createStoryRootEvent({
+  id,
+  title,
+  url,
+  canonical_url,
+  author_name,
+  tags,
+}) {
   return axios.post(
     env.BF_QUEUES_SERVICE_URL + "/add-job/publish-story-to-nostr",
     {
-      story: { id, title, url, canonical_url, author_name },
+      story: { id, title, url, canonical_url, author_name, tags },
       callback_url: env.FUNCTIONS_URL + "/on-job-success",
     },
     {
