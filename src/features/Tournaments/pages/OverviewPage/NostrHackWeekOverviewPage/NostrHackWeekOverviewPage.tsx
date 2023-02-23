7af8da430350e1c12c009c35c97076b7a446ea5d
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-02-23 13:06:12 +0400

    styles (tournament): fix incorrect margin judges card

--- src/features/Tournaments/pages/OverviewPage/NostrHackWeekOverviewPage/NostrHackWeekOverviewPage.tsx
+++ src/features/Tournaments/pages/OverviewPage/NostrHackWeekOverviewPage/NostrHackWeekOverviewPage.tsx
@@ -48,16 +48,23 @@ export default function NostrHackWeekOverviewPage() {
       </Card>
       <Card
         onlyMd
-        className="flex flex-col gap-42 bg-white max-md:-mx-16 max-md:-mt-24 px-16 mt-16 mb-16"
+        className="flex flex-col gap-42 bg-white max-md:-mx-16 px-16 max-md:py-16 md:mt-16"
       >
         <FAQsSection faqs={tournamentDetails.faqs} />
       </Card>
       <Card
         onlyMd
-        className="flex flex-col gap-42 bg-white max-md:-mx-16 max-md:-mt-24 px-16 mt-16 mb-80"
+        className="flex flex-col gap-42 bg-white max-md:-mx-16 px-16 max-md:py-16 md:mt-16 mb-80"
       >
-        <h2 className='text-body1 font-bolder text-gray-900 mb-4'>Community Partners</h2>
-        <p><img className="w-full" src="https://imagedelivery.net/wyrwp3c-j0gDDUWgnE7lig/632c24ae-a49f-41e6-a431-871fbb8ff500/public" /></p>
+        <h2 className="text-body1 font-bolder text-gray-900 mb-4">
+          Community Partners
+        </h2>
+        <p>
+          <img
+            className="w-full"
+            src="https://imagedelivery.net/wyrwp3c-j0gDDUWgnE7lig/632c24ae-a49f-41e6-a431-871fbb8ff500/public"
+          />
+        </p>
       </Card>
     </>
   );
