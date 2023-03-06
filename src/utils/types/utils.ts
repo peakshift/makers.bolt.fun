c9c8d6a8424f1e3c8ae35eaec7ee6cb7b3b8e89d
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-03-06 15:11:18 +0400

    chore (types): rename generic

--- src/utils/types/utils.ts
+++ src/utils/types/utils.ts
@@ -1,11 +1,15 @@
-export type UnionToObjectKeys<O, Key extends keyof O, Value = string> = { [EE in NonNullable<O[Key]> extends string ? NonNullable<O[Key]> : never]: Value }
+export type UnionToObjectKeys<O, Key extends keyof O, Value = string> = {
+  [EE in NonNullable<O[Key]> extends string
+    ? NonNullable<O[Key]>
+    : never]: Value;
+};
 
-export type Id<T> = {} & { [P in keyof T]: T[P] } // flatens out the types to make them more readable can be removed
+export type Prettify<T> = {} & { [P in keyof T]: T[P] }; // flatens out the types to make them more readable can be removed
 
 export type RemoveCommonValues<T, TOmit> = {
-    [P in keyof T]: TOmit extends Record<P, infer U> ? Exclude<T[P], U> : T[P]
-}
+  [P in keyof T]: TOmit extends Record<P, infer U> ? Exclude<T[P], U> : T[P];
+};
 
-export type ValueOf<T> = Id<T[keyof T]>;
+export type ValueOf<T> = Prettify<T[keyof T]>;
 
-export type OmitId<T, Id extends string = 'id'> = Omit<T, Id>
+export type OmitId<T, Id extends string = "id"> = Omit<T, Id>;
