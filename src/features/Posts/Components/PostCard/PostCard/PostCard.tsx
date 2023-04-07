1ba950c053eafb4b7b3e09894ff6122e6d0115fe
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-04-07 15:11:59 +0400

    feat: add comments count & preview to story cards in feed

--- src/features/Posts/Components/PostCard/PostCard/PostCard.tsx
+++ src/features/Posts/Components/PostCard/PostCard/PostCard.tsx
@@ -1,23 +1,30 @@
-import { isStory, isBounty, isQuestion } from "src/features/Posts/types"
-import BountyCard, { BountyCardType } from "../BountyCard/BountyCard"
-import QuestionCard, { QuestionCardType } from "../QuestionCard/QuestionCard"
-import StoryCard, { StoryCardType } from "../StoryCard/StoryCard"
+import { PostsToComments } from "src/features/Posts/pages/FeedPage/useFeedComments";
+import { isStory, isBounty, isQuestion } from "src/features/Posts/types";
+import BountyCard, { BountyCardType } from "../BountyCard/BountyCard";
+import QuestionCard, { QuestionCardType } from "../QuestionCard/QuestionCard";
+import StoryCard, { StoryCardType } from "../StoryCard/StoryCard";
 
 export type PostCardType = StoryCardType | QuestionCardType | BountyCardType;
 
 type Props = {
-  post: PostCardType
-}
+  post: PostCardType;
+  postsToComments: PostsToComments;
+};
 
-export default function PostCard({ post }: Props) {
+export default function PostCard({ post, postsToComments }: Props) {
   if (isStory(post))
-    return <StoryCard story={post} />
+    return (
+      <StoryCard
+        story={post}
+        comments={
+          post.nostr_event_id ? postsToComments[post.nostr_event_id] : undefined
+        }
+      />
+    );
 
-  if (isBounty(post))
-    return <BountyCard bounty={post} />
+  if (isBounty(post)) return <BountyCard bounty={post} />;
 
-  if (isQuestion(post))
-    return <QuestionCard question={post} />
+  if (isQuestion(post)) return <QuestionCard question={post} />;
 
-  return null
+  return null;
 }
--- src/graphql/index.tsx
+++ src/graphql/index.tsx
@@ -1100,7 +1100,7 @@ export type FeedQueryVariables = Exact<{
 }>;
 
 
-export type FeedQuery = { __typename?: 'Query', getFeed: Array<{ __typename?: 'Bounty', id: number, title: string, createdAt: any, excerpt: string, votes_count: number, type: string, cover_image: string | null, deadline: string, reward_amount: number, applicants_count: number, author: { __typename?: 'Author', id: number, name: string, avatar: string, join_date: any }, tags: Array<{ __typename?: 'Tag', id: number, title: string }> } | { __typename?: 'Question', id: number, title: string, createdAt: any, excerpt: string, votes_count: number, type: string, author: { __typename?: 'Author', id: number, name: string, avatar: string, join_date: any }, tags: Array<{ __typename?: 'Tag', id: number, title: string }> } | { __typename?: 'Story', id: number, title: string, createdAt: any, excerpt: string, votes_count: number, type: string, cover_image: string | null, comments_count: number, author: { __typename?: 'Author', id: number, name: string, avatar: string, join_date: any }, tags: Array<{ __typename?: 'Tag', id: number, title: string }>, project: { __typename?: 'Project', id: number, title: string, thumbnail_image: string | null, hashtag: string } | null }> };
+export type FeedQuery = { __typename?: 'Query', getFeed: Array<{ __typename?: 'Bounty', id: number, title: string, createdAt: any, excerpt: string, votes_count: number, type: string, cover_image: string | null, deadline: string, reward_amount: number, applicants_count: number, author: { __typename?: 'Author', id: number, name: string, avatar: string, join_date: any }, tags: Array<{ __typename?: 'Tag', id: number, title: string }> } | { __typename?: 'Question', id: number, title: string, createdAt: any, excerpt: string, votes_count: number, type: string, author: { __typename?: 'Author', id: number, name: string, avatar: string, join_date: any }, tags: Array<{ __typename?: 'Tag', id: number, title: string }> } | { __typename?: 'Story', id: number, title: string, createdAt: any, excerpt: string, votes_count: number, type: string, cover_image: string | null, comments_count: number, nostr_event_id: string | null, author: { __typename?: 'Author', id: number, name: string, avatar: string, join_date: any }, tags: Array<{ __typename?: 'Tag', id: number, title: string }>, project: { __typename?: 'Project', id: number, title: string, thumbnail_image: string | null, hashtag: string } | null }> };
 
 export type PostDetailsQueryVariables = Exact<{
   id: Scalars['Int'];
@@ -2226,6 +2226,7 @@ export const FeedDocument = gql`
       type
       cover_image
       comments_count
+      nostr_event_id
       project {
         id
         title
