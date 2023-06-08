f9fb2c6363cf53cc37131b8d49e1f3e29366d78a
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-06-08 16:33:54 +0400

    update: add cta

--- src/features/Home/pages/LandingPage/LandingPage.tsx
+++ src/features/Home/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,11 @@
 import OgTags from "src/Components/OgTags/OgTags";
 import BgImage from "./assets/bg.jpg";
+import ConfettiImage from "./assets/confetti.png";
+import CreateProfileImage from "./assets/create-profile.png";
 import Button from "src/Components/Button/Button";
 import resourcesData from "./resources.json";
 import { FaChevronRight } from "react-icons/fa";
+import { PAGES_ROUTES, createRoute } from "src/utils/routing";
 
 export default function LandingPage() {
   return (
@@ -35,13 +38,17 @@ export default function LandingPage() {
                 Now we celebrate 10 years of 0 downtime and fast with layer 2!
               </p>
               <div className="flex flex-wrap gap-20">
-                <Button color="none" className="bg-gray-700 text-white px-32">
+                <Button
+                  href={createRoute({ type: "write-story" })}
+                  color="none"
+                  className="bg-gray-700 text-white px-32"
+                >
                   Introduce yourself 🚀
                 </Button>
               </div>
             </div>
           </header>
-          <button className="my-80 px-32 py-24 bg-gray-900 text-white flex gap-16 text-left rounded-16">
+          <button className="my-32 px-32 py-24 bg-gray-900 text-white flex gap-16 text-left rounded-16">
             <div>
               <p className="text-body2 font-bolder">AI4ALL Hackathon ⚡️</p>
               <p className="">
@@ -92,6 +99,42 @@ export default function LandingPage() {
               </ul>
             </section>
           ))}
+          <section className="bg-gray-800 rounded-16 p-32 text-center  relative isolate">
+            <img
+              src={ConfettiImage}
+              alt=""
+              className="absolute bottom-0 left-1/2 -translate-x-1/2 z-0"
+            />
+            <div className="flex flex-col gap-12 relative">
+              <h2 className="text-body1 font-bolder bg-gradient-to-r from-blue-500 to-violet-400 bg-clip-text text-transparent ">
+                Join the community
+              </h2>
+              <p className="text-body3 text-white">
+                Proof of work starts with you. Share your work, build in public,
+                win bounties and take part in hackathons and tournaments! Sign
+                up and introduce yourself to the community.
+              </p>
+              <img
+                src={CreateProfileImage}
+                alt=""
+                className="self-center max-w-[300px]"
+              />
+              <Button
+                href={PAGES_ROUTES.auth.login}
+                color="primary"
+                className="self-center !px-64 -mt-40"
+              >
+                Sign up
+              </Button>
+              <p className="text-gray-300">
+                Read more about the{" "}
+                <a href="#" className="underline">
+                  FUN
+                </a>{" "}
+                community
+              </p>
+            </div>
+          </section>
         </div>
       </main>
     </>
