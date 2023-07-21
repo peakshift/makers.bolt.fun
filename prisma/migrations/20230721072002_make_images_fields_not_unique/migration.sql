b0848e49572151ff21ae1aa67fb472f06061ead5
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-07-21 14:01:13 +0400

    feat (db): add db migrations

---
+++ prisma/migrations/20230721072002_make_images_fields_not_unique/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Tournament_cover_image_id_key";
+
+-- DropIndex
+DROP INDEX "Tournament_thumbnail_image_id_key";
+
+-- DropIndex
+DROP INDEX "TournamentEvent_image_id_key";
+
+-- DropIndex
+DROP INDEX "TournamentJudge_avatar_id_key";
