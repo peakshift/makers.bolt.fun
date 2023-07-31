e9971b69e6406685d12290545b9e00565edbf67e
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-07-31 19:41:07 +0400

    fix (navbar): fix notifications list style bug

--- src/Components/Navbar/NotificationsList/NotificationsList.tsx
+++ src/Components/Navbar/NotificationsList/NotificationsList.tsx
@@ -41,8 +41,15 @@ export default function NotificationsList(props: Props) {
     { loading: isUpdatingLastSeenTime },
   ] = useUpdateLastSeenNotificationTimeMutation();
 
+  const isLoading = notifications.length === 0 && !isEmpty;
+  const hasNewNotifications =
+    notifications.length > 0 &&
+    notifications[0].created_at > lastSeenNotificationUnixTime;
+
+  let content = <></>;
+
   if (!myUser.primary_nostr_key)
-    return (
+    content = (
       <div className="flex flex-col items-center py-16 gap-16">
         <p className="text-gray-600 text-center ">
           To see notifications, please link your nostr public key first.
@@ -56,57 +63,45 @@ export default function NotificationsList(props: Props) {
         </Button>
       </div>
     );
-
-  const isLoading = notifications.length === 0 && !isEmpty;
-
-  if (isLoading)
-    return (
-      <Menu
-        align="end"
-        arrow
-        menuClassName={props.menuClassName}
-        menuButton={props.renderOpenListButton({})}
-        aria-label="Notifications"
-      >
-        <div aria-label="Loading">
-          <MenuItem className="!p-16 flex gap-16 hover:bg-gray-100 !rounded-12 text-body5 ">
-            <div className="flex gap-16 items-start w-full">
-              <Skeleton circle width="32px" height="32px" />
-              <div className="flex-1">
-                <p className="text-gray-900 font-bold ">
-                  <Skeleton width="80%" />
-                </p>
-                <p className="text-body6 text-gray-500 font-medium">
-                  <Skeleton width="5ch" />
-                </p>
-                <p className="line-clamp-2 text-gray-600 mt-4">
-                  <Skeleton /> <Skeleton />
-                </p>
-              </div>
+  else if (isLoading)
+    content = (
+      <div aria-label="Loading">
+        <MenuItem className="!p-16 flex gap-16 hover:bg-gray-100 !rounded-12 text-body5 ">
+          <div className="flex gap-16 items-start w-full">
+            <Skeleton circle width="32px" height="32px" />
+            <div className="flex-1">
+              <p className="text-gray-900 font-bold ">
+                <Skeleton width="80%" />
+              </p>
+              <p className="text-body6 text-gray-500 font-medium">
+                <Skeleton width="5ch" />
+              </p>
+              <p className="line-clamp-2 text-gray-600 mt-4">
+                <Skeleton /> <Skeleton />
+              </p>
             </div>
-          </MenuItem>
-          <MenuItem className="!p-16 flex gap-16 hover:bg-gray-100 !rounded-12 text-body5 ">
-            <div className="flex gap-16 items-start w-full">
-              <Skeleton circle width="32px" height="32px" />
-              <div className="flex-1">
-                <p className="text-gray-900 font-bold ">
-                  <Skeleton width="80%" />
-                </p>
-                <p className="text-body6 text-gray-500 font-medium">
-                  <Skeleton width="5ch" />
-                </p>
-                <p className="line-clamp-2 text-gray-600 mt-4">
-                  <Skeleton /> <Skeleton />
-                </p>
-              </div>
+          </div>
+        </MenuItem>
+        <MenuItem className="!p-16 flex gap-16 hover:bg-gray-100 !rounded-12 text-body5 ">
+          <div className="flex gap-16 items-start w-full">
+            <Skeleton circle width="32px" height="32px" />
+            <div className="flex-1">
+              <p className="text-gray-900 font-bold ">
+                <Skeleton width="80%" />
+              </p>
+              <p className="text-body6 text-gray-500 font-medium">
+                <Skeleton width="5ch" />
+              </p>
+              <p className="line-clamp-2 text-gray-600 mt-4">
+                <Skeleton /> <Skeleton />
+              </p>
             </div>
-          </MenuItem>
-        </div>
-      </Menu>
+          </div>
+        </MenuItem>
+      </div>
     );
-
-  if (!isLoading && notifications.length === 0)
-    return (
+  else if (!isLoading && notifications.length === 0)
+    content = (
       <div
         className="flex flex-col items-center py-16 gap-16"
         aria-label="notifications empty"
@@ -114,29 +109,132 @@ export default function NotificationsList(props: Props) {
         <p className="text-gray-600 text-center ">Nothing to be seen here...</p>
       </div>
     );
+  else {
+    const onSeenNotifications = () => {
+      if (!hasNewNotifications) return;
+      if (isUpdatingLastSeenTime) return;
 
-  const hasNewNotifications =
-    notifications[0].created_at > lastSeenNotificationUnixTime;
-
-  const onSeenNotifications = () => {
-    if (!hasNewNotifications) return;
-    if (isUpdatingLastSeenTime) return;
-
-    updateLastSeenNotificationTimeMutation({
-      variables: {
-        timestamp: new Date(notifications[0].created_at * 1000).toISOString(),
-      },
-      onCompleted: (data) => {
-        if (data.updateLastSeenNotificationTime)
-          dispatch(
-            updateMeData({
-              last_seen_notification_time:
-                data.updateLastSeenNotificationTime.last_seen_notification_time,
-            })
+      updateLastSeenNotificationTimeMutation({
+        variables: {
+          timestamp: new Date(notifications[0].created_at * 1000).toISOString(),
+        },
+        onCompleted: (data) => {
+          if (data.updateLastSeenNotificationTime)
+            dispatch(
+              updateMeData({
+                last_seen_notification_time:
+                  data.updateLastSeenNotificationTime
+                    .last_seen_notification_time,
+              })
+            );
+        },
+      });
+    };
+    content = (
+      <>
+        <NotificationsSeen onNotificationsSeen={onSeenNotifications} />
+        {notifications.slice(0, numberOfEventsToRender).map((notification) => {
+          const isNew = notification.created_at > lastSeenNotificationUnixTime;
+          return (
+            <MenuItem
+              key={notification.id}
+              href={notification.url}
+              onClick={(e) => {
+                e.syntheticEvent.preventDefault();
+                navigate(new URL(notification.url).pathname);
+              }}
+              className={`!p-16 flex gap-16 !rounded-12 text-body5 ${
+                isNew
+                  ? "bg-orange-100 hover:bg-orange-200"
+                  : "hover:bg-gray-200 "
+              }`}
+            >
+              {isNew && <p className="sr-only">New Notification</p>}
+              {notification.type === "comment-on-post" && (
+                <div className="flex gap-16 items-start w-full">
+                  <Avatar src={notification.user.image} width={32} />
+                  <div className="min-w-0">
+                    <p className="text-gray-900 font-bold ">
+                      {trimText(notification.user.name, 12)} commented on your
+                      post
+                    </p>
+                    <p className="text-body6 text-gray-500 font-medium">
+                      {dayjs(notification.created_at * 1000).from(new Date())}
+                    </p>
+                    <p className="line-clamp-2 text-gray-600 mt-4">
+                      {notification.commentText}
+                    </p>
+                  </div>
+                </div>
+              )}
+              {notification.type === "reply-on-comment" && (
+                <div className="flex gap-16 items-start w-full">
+                  <Avatar src={notification.user.image} width={32} />
+                  <div className="min-w-0">
+                    <p className="text-gray-900 font-bold ">
+                      {trimText(notification.user.name, 12)} replied on your
+                      comment
+                    </p>
+                    <p className="text-body6 text-gray-500 font-medium">
+                      {dayjs(notification.created_at * 1000).from(new Date())}
+                    </p>
+                    <p className="line-clamp-2 text-gray-600 mt-4">
+                      {notification.replyText}
+                    </p>
+                  </div>
+                </div>
+              )}
+              {notification.type === "mention-in-post" && (
+                <div className="flex gap-16 items-start w-full">
+                  <Avatar src={notification.user.image} width={32} />
+                  <div className="min-w-0">
+                    <p className="text-gray-900 font-bold ">
+                      {trimText(notification.user.name, 12)} mentioned you in a
+                      post
+                    </p>
+                    <p className="text-body6 text-gray-500 font-medium">
+                      {dayjs(notification.created_at * 1000).from(new Date())}
+                    </p>
+                    <p className="line-clamp-2 text-gray-600 mt-4">
+                      {notification.postTitle}
+                    </p>
+                  </div>
+                </div>
+              )}
+            </MenuItem>
           );
-      },
-    });
-  };
+        })}
+        {notifications.length > numberOfEventsToRender && (
+          <FocusableItem>
+            {({ ref }) => (
+              <Button
+                ref={ref}
+                color="gray"
+                variant="text"
+                fullWidth
+                onClick={() =>
+                  setNumberOfEventsToRender((v) => v + RENDER_PER_PAGE)
+                }
+              >
+                Load More
+              </Button>
+            )}
+          </FocusableItem>
+        )}
+      </>
+    );
+  }
+  // return (
+  //   <Menu
+  //     align="end"
+  //     arrow
+  //     menuClassName={props.menuClassName}
+  //     menuButton={props.renderOpenListButton({})}
+  //     aria-label="Notifications"
+  //   >
+
+  //   </Menu>
+  // );
 
   return (
     <Menu
@@ -146,93 +244,7 @@ export default function NotificationsList(props: Props) {
       menuButton={props.renderOpenListButton({ hasNewNotifications })}
       aria-label="Notifications List"
     >
-      <NotificationsSeen onNotificationsSeen={onSeenNotifications} />
-      {notifications.slice(0, numberOfEventsToRender).map((notification) => {
-        const isNew = notification.created_at > lastSeenNotificationUnixTime;
-        return (
-          <MenuItem
-            key={notification.id}
-            href={notification.url}
-            onClick={(e) => {
-              e.syntheticEvent.preventDefault();
-              navigate(new URL(notification.url).pathname);
-            }}
-            className={`!p-16 flex gap-16 !rounded-12 text-body5 ${
-              isNew ? "bg-orange-100 hover:bg-orange-200" : "hover:bg-gray-200 "
-            }`}
-          >
-            {isNew && <p className="sr-only">New Notification</p>}
-            {notification.type === "comment-on-post" && (
-              <div className="flex gap-16 items-start w-full">
-                <Avatar src={notification.user.image} width={32} />
-                <div className="min-w-0">
-                  <p className="text-gray-900 font-bold ">
-                    {trimText(notification.user.name, 12)} commented on your
-                    post
-                  </p>
-                  <p className="text-body6 text-gray-500 font-medium">
-                    {dayjs(notification.created_at * 1000).from(new Date())}
-                  </p>
-                  <p className="line-clamp-2 text-gray-600 mt-4">
-                    {notification.commentText}
-                  </p>
-                </div>
-              </div>
-            )}
-            {notification.type === "reply-on-comment" && (
-              <div className="flex gap-16 items-start w-full">
-                <Avatar src={notification.user.image} width={32} />
-                <div className="min-w-0">
-                  <p className="text-gray-900 font-bold ">
-                    {trimText(notification.user.name, 12)} replied on your
-                    comment
-                  </p>
-                  <p className="text-body6 text-gray-500 font-medium">
-                    {dayjs(notification.created_at * 1000).from(new Date())}
-                  </p>
-                  <p className="line-clamp-2 text-gray-600 mt-4">
-                    {notification.replyText}
-                  </p>
-                </div>
-              </div>
-            )}
-            {notification.type === "mention-in-post" && (
-              <div className="flex gap-16 items-start w-full">
-                <Avatar src={notification.user.image} width={32} />
-                <div className="min-w-0">
-                  <p className="text-gray-900 font-bold ">
-                    {trimText(notification.user.name, 12)} mentioned you in a
-                    post
-                  </p>
-                  <p className="text-body6 text-gray-500 font-medium">
-                    {dayjs(notification.created_at * 1000).from(new Date())}
-                  </p>
-                  <p className="line-clamp-2 text-gray-600 mt-4">
-                    {notification.postTitle}
-                  </p>
-                </div>
-              </div>
-            )}
-          </MenuItem>
-        );
-      })}
-      {notifications.length > numberOfEventsToRender && (
-        <FocusableItem>
-          {({ ref }) => (
-            <Button
-              ref={ref}
-              color="gray"
-              variant="text"
-              fullWidth
-              onClick={() =>
-                setNumberOfEventsToRender((v) => v + RENDER_PER_PAGE)
-              }
-            >
-              Load More
-            </Button>
-          )}
-        </FocusableItem>
-      )}
+      {content}
     </Menu>
   );
 }
