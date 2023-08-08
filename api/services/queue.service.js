186067cb602044edc9ea8039fff2217faa3cd77d
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-08-08 16:31:39 +0400

    update (api): update queue service URLs

--- api/services/queue.service.js
+++ api/services/queue.service.js
@@ -10,45 +10,62 @@ function createStoryRootEvent({
   author_nostr_pubkey,
   tags,
 }) {
-  return axios.post(
-    env.BF_QUEUES_SERVICE_URL + "/add-job/publish-story-to-nostr",
-    {
-      story: {
-        id,
-        title,
-        url,
-        canonical_url,
-        author_name,
-        author_nostr_pubkey,
-        tags,
-      },
-      callback_url: env.FUNCTIONS_URL + "/on-job-success",
+  return callQueueApi("/add-job/nostr/publish-story-event", {
+    story: {
+      id,
+      title,
+      url,
+      canonical_url,
+      author_name,
+      author_nostr_pubkey,
+      tags,
     },
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${env.BF_QUEUES_SERVICE_USERNAME}:${env.BF_QUEUES_SERVICE_PASS}`
-        ).toString("base64")}`,
-      },
-    }
-  );
+    callback_url: env.FUNCTIONS_URL + "/on-job-success",
+  });
 }
 
 function publishProfileVerifiedEvent({ event }) {
-  return axios.post(
-    env.BF_QUEUES_SERVICE_URL +
-      "/add-job/publish-profile-verification-to-nostr",
-    {
-      event,
+  return callQueueApi("/add-job/nostr/publish-profile-verification-event", {
+    event,
+  });
+}
+
+function newUserRegisteredInTournament({
+  user_id,
+  user_name,
+  tournament_id,
+  email,
+}) {
+  return callQueueApi("/add-job/emails/new-user-registered-in-tournament", {
+    user_id,
+    user_name,
+    tournament_id,
+    email,
+  });
+}
+
+function newProjectSubmittedInTournament({
+  user_id,
+  project_id,
+  tournament_id,
+  track_id,
+}) {
+  return callQueueApi("/add-job/emails/new-project-submitted-to-tournament", {
+    user_id,
+    project_id,
+    tournament_id,
+    track_id,
+  });
+}
+
+function callQueueApi(url, data) {
+  return axios.post(env.BF_QUEUES_SERVICE_URL + url, data, {
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${env.BF_QUEUES_SERVICE_USERNAME}:${env.BF_QUEUES_SERVICE_PASS}`
+      ).toString("base64")}`,
     },
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${env.BF_QUEUES_SERVICE_USERNAME}:${env.BF_QUEUES_SERVICE_PASS}`
-        ).toString("base64")}`,
-      },
-    }
-  );
+  });
 }
 
 const queueService = { createStoryRootEvent, publishProfileVerifiedEvent };
