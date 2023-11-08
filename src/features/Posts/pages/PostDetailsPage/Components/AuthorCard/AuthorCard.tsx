0611c5c183c95ecf99fd60685b79e84bbe9bb0cd
MTG\mtg09 <50504183+MTG2000@users.noreply.github.com>
2023-11-08 13:25:03 +0400

    update: wrap link around author card's image & name

--- src/features/Posts/pages/PostDetailsPage/Components/AuthorCard/AuthorCard.tsx
+++ src/features/Posts/pages/PostDetailsPage/Components/AuthorCard/AuthorCard.tsx
@@ -8,30 +8,44 @@ import { trimText } from "src/utils/helperFunctions";
 import { createRoute } from "src/utils/routing";
 
 interface Props {
-    author: Pick<Author,
-        | 'id'
-        | 'name'
-        | 'avatar'
-        | 'join_date'>
+  author: Pick<Author, "id" | "name" | "avatar" | "join_date">;
 }
 
 export default function AuthorCard({ author }: Props) {
-    return (
-        <Card>
-            <div className='flex gap-8'>
-                <Avatar width={48} src={author.avatar} />
-                <div className="overflow-hidden">
-                    <p className={`'text-body4' text-black font-medium overflow-hidden text-ellipsis whitespace-nowrap`}>{trimText(author.name, 333)}</p>
-                    <p className={`text-body6 text-gray-600`}>Joined on {dayjs(author.join_date).format('MMMM DD, YYYY')}</p>
-                </div>
-            </div>
-            <Button
-                fullWidth
-                href={createRoute({ type: 'profile', id: author.id, username: author.name })}
-                color="primary"
-                className="mt-16">
-                Maker's Profile
-            </Button>
-        </Card>
-    )
+  return (
+    <Card>
+      <Link
+        to={createRoute({
+          type: "profile",
+          id: author.id,
+          username: author.name,
+        })}
+        className="flex gap-8"
+      >
+        <Avatar width={48} src={author.avatar} />
+        <div className="overflow-hidden">
+          <p
+            className={`'text-body4' text-black font-medium overflow-hidden text-ellipsis whitespace-nowrap`}
+          >
+            {trimText(author.name, 333)}
+          </p>
+          <p className={`text-body6 text-gray-600`}>
+            Joined on {dayjs(author.join_date).format("MMMM DD, YYYY")}
+          </p>
+        </div>
+      </Link>
+      <Button
+        fullWidth
+        href={createRoute({
+          type: "profile",
+          id: author.id,
+          username: author.name,
+        })}
+        color="primary"
+        className="mt-16"
+      >
+        Maker's Profile
+      </Button>
+    </Card>
+  );
 }
